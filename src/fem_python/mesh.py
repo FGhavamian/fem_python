3870fdf3f125f23979820fd631533b7e6afb2773
Fariborz Ghavamian <fari.ghavamian@gmail.com>
2024-12-08 13:46:20 +0100

    2d minimal mesh

--- src/fem_python/mesh.py
+++ src/fem_python/mesh.py
@@ -2,9 +2,17 @@ from fem_python import config
 
 
 class FEMMesh:
+    """The 2D mesh of a 1d bar looks like this:
+    3---4---5
+    | 0 | 1 |
+    0---1---2
+
+    the height is 1. And the lengths is 1. Let's say the depth is also 1.
+    """
+
     def __init__(self):
         self.num_nodes = self._get_num_nodes()
-        self.element_length = self._get_elements_lengths()
+        self.element_depths = self._get_elements_depths()
         self.element_to_nodes_mapping = self._assemble_element_to_node()
         self.node_coordinates = self._assemble_node_coordinates()
         self.num_integration_points_needed = self._get_num_integration_points_needed()
@@ -29,37 +37,47 @@ class FEMMesh:
             return 2
 
     def _get_num_nodes(self):
-        if config.element_type == "linear":
-            num_nodes = config.num_elements + 1
-        if config.element_type == "quad":
-            num_nodes = 3 + 2 * (config.num_elements - 1)
+        return 6
+        # if config.element_type == "linear":
+        #     num_nodes = config.num_elements + 1
+        # if config.element_type == "quad":
+        #     num_nodes = 3 + 2 * (config.num_elements - 1)
 
         return num_nodes
 
     def _assemble_element_to_node(self):
-        element_to_node = {}
-        for e in range(config.num_elements):
-            if config.element_type == "linear":
-                element_to_node[e] = list(range(e, e + 2))
-            if config.element_type == "quad":
-                element_to_node[e] = list(range(2 * e, 2 * e + 3))
+        element_to_node = {0: [0, 1, 4, 3], 1: [1, 2, 5, 4]}
+        # element_to_node = {}
+        # for e in range(config.num_elements):
+        #     if config.element_type == "linear":
+        #         element_to_node[e] = list(range(e, e + 2))
+        #     if config.element_type == "quad":
+        #         element_to_node[e] = list(range(2 * e, 2 * e + 3))
 
         return element_to_node
 
     def _assemble_node_coordinates(self):
-        node_coords = {}
-        for n in range(self.num_nodes):
-            if config.element_type == "linear":
-                distance_between_nodes = self.element_length
-                node_coords[n] = n * distance_between_nodes
-            if config.element_type == "quad":
-                distance_between_nodes = self.element_length / 2
-                node_coords[n] = n * distance_between_nodes
+        node_coords = {
+            0: (0, 0),
+            1: (0.5, 0),
+            2: (1, 0),
+            3: (0, 1),
+            4: (0.5, 1),
+            5: (1, 1),
+        }
+        # node_coords = {}
+        # for n in range(self.num_nodes):
+        #     if config.element_type == "linear":
+        #         distance_between_nodes = self.element_length
+        #         node_coords[n] = n * distance_between_nodes
+        #     if config.element_type == "quad":
+        #         distance_between_nodes = self.element_length / 2
+        #         node_coords[n] = n * distance_between_nodes
 
         return node_coords
 
-    def _get_elements_lengths(self):
-        return config.bar_length / config.num_elements
+    def _get_elements_depths(self):
+        return 1
 
 
 if __name__ == "__main__":

7b4f03e2de9593b52384a7bdf6ea080437dfc1da
Fariborz Ghavamian <fari.ghavamian@gmail.com>
2024-12-26 00:23:26 +0100

    keep track of force displacement curve, plot it

--- src/fem_python/main.py
+++ src/fem_python/main.py
@@ -1,13 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from fem_python.fem import (
-    make_stiffness_matrix,
+    make_stiffness_matrix_and_internal_force_vector,
     solve,
     apply_dirichlet_boundary_condition,
     apply_neuman_boundary_condition,
 )
 from fem_python.mesh.mesh import FEMMesh
-from fem_python.postprocess.postprocess import (
+from fem_python.postprocess import (
     write_to_vtk,
     compute_stress_and_strain_at_nodes,
     compute_displacement_at_nodes,
@@ -21,36 +22,80 @@ fem_mesh = FEMMesh()
 # We apply the external force or displacement at uniform proportions.
 # For instance, if the presribed displacement is 1 and the number of time steps is 10,
 # then the displacement at each time step will be 0.1.
-
-force_step = np.array(config.uniform_force_at_right_boundary) / config.num_time_steps
-displacement_step_x = (
+force_step_increment = (
+    np.array(config.uniform_force_at_right_boundary) / config.num_time_steps
+)
+displacement_step_x_increment = (
     config.prescribed_displacement_at_right_boundary_x / config.num_time_steps
 )
 
+# We initiate the displacement vector here because, the function make_stiffness_matrix_and_internal_force_vector
+# requires the displacement vector to compute strain. A the first time step, the displacement vector is zero hence,
+# the strain is zero. After that, the displacement vector is updated at each time step and hence, the strain takes a
+# none-zero value.
+displacement_vec = np.zeros((fem_mesh.num_nodes * 2,))
+
+force_step = np.zeros_like(force_step_increment)
+displacement_step_x = 0
+
+# It is usually of interest to keep track of a quantity of interest. In the 1D bar problem,
+# we are interested in the force and displacement at the right boundary. We keep track of
+# how the internal force increases as we increase the displacement.
+force_displacement_right_boundary = {"force": [], "displacement": []}
+
 for t in range(config.num_time_steps):
+    stiffness_mat, internal_force_vec = make_stiffness_matrix_and_internal_force_vector(
+        fem_mesh, displacement_vec
+    )
 
-    # make stiffness vector
-    stiffness_mat = make_stiffness_matrix(fem_mesh)
+    force_step += force_step_increment
+    displacement_step_x += displacement_step_x_increment
 
-    # apply neuman boundary condition
     force_vec = apply_neuman_boundary_condition(fem_mesh, force_step)
 
-    # apply dirichlet boundary condition
     stiffness_mat, force_vec = apply_dirichlet_boundary_condition(
         fem_mesh, stiffness_mat, force_vec, displacement_step_x
     )
 
-    # solve for displacement vector
     displacement_vec = solve(stiffness_mat, force_vec)
 
+    # Collecting the force and displacement at the right boundary. The force and the displacement vectors on the right boundary
+    # are averaged out. This is because the right boundary is made up of multiple nodes. This is again not the most accurate way of
+    # computing the force but, it is quite simple.
+    elements = fem_mesh.boundary_connectivity_matrices["right"]
+
+    dofs_ux = []
+    for nodes in elements:
+        for node in nodes:
+            dof_x = 2 * node
+            dofs_ux.append(dof_x)
+
+    force_right_boundary = np.mean(force_vec[np.ix_(dofs_ux)])
+    displacement_right_boundary = np.mean(displacement_vec[np.ix_(dofs_ux)])
+
+    force_displacement_right_boundary["force"].append(force_right_boundary)
+    force_displacement_right_boundary["displacement"].append(
+        displacement_right_boundary
+    )
 
 stress_vec, strain_vec = compute_stress_and_strain_at_nodes(displacement_vec, fem_mesh)
 displacement_vec = compute_displacement_at_nodes(displacement_vec, fem_mesh)
+internal_force_vec = compute_displacement_at_nodes(internal_force_vec, fem_mesh)
 
 vecs_dict = {
     "stress": stress_vec,
     "strain": strain_vec,
     "displacement": displacement_vec,
+    "internal_force": internal_force_vec,
 }
 
 write_to_vtk(vecs_dict, fem_mesh)
+
+plt.plot(
+    force_displacement_right_boundary["displacement"],
+    force_displacement_right_boundary["force"],
+)
+plt.xlabel("Displacement at right boundary")
+plt.ylabel("Force at right boundary")
+plt.title("1D Bar Problem")
+plt.show()

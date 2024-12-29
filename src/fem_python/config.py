02c9404efba70d5257613390ba93f5419841f49b
Fariborz Ghavamian <fari.ghavamian@gmail.com>
2024-12-29 23:08:19 +0100

    plate with hole mesh

--- src/fem_python/config.py
+++ src/fem_python/config.py
@@ -1,18 +1,18 @@
 num_time_steps = 10
 max_num_nr_iterations = 4
 
-material_model_name = "nonlinear_elastic"
+material_model_name = "linear_elastic"
 bar_elasticity_module = 1
 bar_poission_ratio = 0
 plane_stress = False
 
 element_type = "Q4"
-mesh_file_path = "meshes/1d_bar.msh"
+mesh_file_path = "meshes/plate_w_hole.msh"
 
 right_boundary_node_tag = 11
 left_boundary_node_tag = 12
+bottom_boundary_node_tag = 13
 
-uniform_force_at_right_boundary = [0, 0]
 prescribed_displacement_at_right_boundary_x = 1
 
 num_integration_points = 2
